9f3c9ab56892170771400fde704c79768e1cef62
Peter Levart <peter.levart@gmail.com>
2026-02-22 12:34:16 +0100

    Add implementation and benchmark test with results

---
+++ src/arc/AcquireReleaseClose.java
@@ -0,0 +1,116 @@
+package arc;
+
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
+import java.util.concurrent.atomic.LongAdder;
+
+public sealed interface AcquireReleaseClose extends AutoCloseable {
+
+    void acquire();
+
+    void release();
+
+    final class Simple implements AcquireReleaseClose {
+        private static final VarHandle ACQUIRE_COUNT;
+
+        static {
+            try {
+                ACQUIRE_COUNT = MethodHandles.lookup().findVarHandle(Simple.class, "acquireCount", int.class);
+            } catch (NoSuchFieldException | IllegalAccessException e) {
+                throw new AssertionError(e);
+            }
+        }
+
+        private volatile int acquireCount;
+
+        @Override
+        public void acquire() {
+            int value;
+            do {
+                value = acquireCount;
+                if (value < 0) {
+                    //segment is not open!
+                    throw new IllegalStateException("Already closed");
+                } else if (value == Integer.MAX_VALUE) {
+                    //overflow
+                    throw new IllegalStateException("Too many acquires");
+                }
+            } while (!ACQUIRE_COUNT.compareAndSet(this, value, value + 1));
+        }
+
+        @Override
+        public void release() {
+            int value;
+            do {
+                value = acquireCount;
+                if (value <= 0) {
+                    //cannot get here - we can't close segment twice
+                    throw new IllegalStateException("Already closed");
+                }
+            } while (!ACQUIRE_COUNT.compareAndSet(this, value, value - 1));
+        }
+
+        @Override
+        public void close() {
+            int value = (int) ACQUIRE_COUNT.compareAndExchange(this, 0, -1);
+            if (value < 0) {
+                throw new IllegalStateException("Already closed");
+            } else if (value > 0) {
+                throw new IllegalStateException("Still acquired by " + value + " threads");
+            }
+        }
+    }
+
+    final class Striped implements AcquireReleaseClose {
+        private static final int OPEN = 0, MAYBE_CLOSING = 1, CLOSED = 2;
+        private static final VarHandle CLOSING_PHASE;
+
+        static {
+            try {
+                CLOSING_PHASE = MethodHandles.lookup().findVarHandle(Striped.class, "closingPhase", int.class);
+            } catch (NoSuchFieldException | IllegalAccessException e) {
+                throw new AssertionError(e);
+            }
+        }
+
+        private volatile int closingPhase;
+        private final LongAdder acquireCount = new LongAdder();
+        private final LongAdder releaseCount = new LongAdder();
+
+        @Override
+        public void acquire() {
+            acquireCount.increment();
+            int cp;
+            while ((cp = closingPhase) != OPEN) {
+                if (cp == CLOSED) {
+                    releaseCount.increment(); // not required, but useful for debugging
+                    throw new IllegalStateException("Already closed");
+                }
+                Thread.onSpinWait();
+            }
+        }
+
+        @Override
+        public void release() {
+            releaseCount.increment();
+        }
+
+        @Override
+        public void close() {
+            int cp;
+            while ((cp = (int) CLOSING_PHASE.compareAndExchange(this, OPEN, MAYBE_CLOSING)) != OPEN) {
+                if (cp == CLOSED) {
+                    throw new IllegalStateException("Already closed");
+                }
+                Thread.onSpinWait();
+            }
+            // reading order is important: 1st RELEASE_COUNT, 2nd ACQUIRE_COUNT
+            long value = -releaseCount.longValue() + acquireCount.longValue();
+            if (value > 0) {
+                closingPhase = OPEN;
+                throw new IllegalStateException("Still acquired by " + value + " threads");
+            }
+            closingPhase = CLOSED;
+        }
+    }
+}
---
+++ src/arc/TestARC.java
@@ -0,0 +1,132 @@
+package arc;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+
+public class TestARC implements AutoCloseable {
+
+    static void main() {
+        int cpus = Runtime.getRuntime().availableProcessors();
+        long loops = 5000000L;
+        try (var arc = new AcquireReleaseClose.Striped()) {
+            testRuns(cpus, loops, arc, 3, 3);
+        }
+    }
+
+    static void testRuns(int cpus, long loops, AcquireReleaseClose arc, int warmups, int measurements) {
+        for (int i = 1; i <= warmups; i++) {
+            System.out.println("** " + arc.getClass().getSimpleName() + ", warmup run #" + i + "...");
+            testRun(cpus, loops, arc);
+        }
+        for (int i = 1; i <= measurements; i++) {
+            System.out.println("** " + arc.getClass().getSimpleName() + ", measure run #"  + i + "...");
+            testRun(cpus, loops, arc);
+        }
+        System.out.println("** end.");
+    }
+
+    static void testRun(int cpus, long loops, AcquireReleaseClose arc) {
+        try (var test = new TestARC()) {
+            // warmup
+            Long referenceNs = null;
+            var concurrencies = concurrencies(cpus);
+            for (int i = 0; i < concurrencies.length; i++) {
+                long ns = test.test(concurrencies[i], loops, arc, referenceNs);
+                if (i == 0) {
+                    referenceNs = ns;
+                }
+            }
+        }
+    }
+
+    private final ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+    private volatile boolean go;
+    private final AtomicInteger ready = new AtomicInteger(0);
+
+    @Override
+    public void close() {
+        executor.shutdown();
+    }
+
+    long test(int concurrency, long loops, AcquireReleaseClose arc, Long referenceNs) {
+        go = false;
+        ready.set(0);
+        var cfs = IntStream
+            .range(0, concurrency)
+            .mapToObj(_ -> CompletableFuture.supplyAsync(
+                () -> {
+                    ready.incrementAndGet();
+                    while (!go) {
+                        Thread.onSpinWait();
+                    }
+                    long counter = 0;
+                    for (long l = 0; l < loops; l++) {
+                        arc.acquire();
+                        try {
+                            counter++;
+                        } finally {
+                            arc.release();
+                        }
+                    }
+                    return counter;
+                },
+                executor
+            ))
+            .toArray(CompletableFuture<?>[]::new);
+
+        while (ready.get() < concurrency) {
+            Thread.onSpinWait();
+        }
+        long t0 = System.nanoTime();
+        go = true;
+        CompletableFuture.allOf(cfs).join();
+        long ns = System.nanoTime() - t0;
+
+        System.out.println(
+            "concurrency: " + concurrency +
+                ", nanos: " + ns +
+                (referenceNs != null ? " (x " + ((double) ns / (double) referenceNs) + ")" : " (x 1.0)")
+        );
+
+        return ns;
+    }
+
+    static int[] concurrencies(int cpus) {
+        var factors = factorize(cpus);
+        var concurrencies = IntStream.builder();
+        int product = 1;
+        concurrencies.add(product);
+        for (var factor : factors) {
+            int p = product;
+            for (int f = 2; f <= factor; f++) {
+                p = product * f;
+                concurrencies.add(p);
+            }
+            product = p;
+        }
+        return concurrencies.build().toArray();
+    }
+
+
+    static int[] factorize(int n) {
+        var factors = IntStream.builder();
+        while (n % 2 == 0) {
+            factors.add(2);
+            n /= 2;
+        }
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+            while (n % i == 0) {
+                factors.add(i);
+                n /= i;
+            }
+        }
+        if (n > 2) {
+            factors.add(n);
+        }
+        return factors.build().toArray();
+    }
+}
